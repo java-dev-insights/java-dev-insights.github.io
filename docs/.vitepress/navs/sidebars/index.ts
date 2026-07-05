fc18503957a4a349bf43c5141892f8fdc0d048a5
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-07-05 18:26:49 +0100

    export individual sidenavs

---
+++ docs/.vitepress/navs/sidebars/index.ts
@@ -0,0 +1,20 @@
+// Home Page
+export { default as SIDEBAR_DEFAULT_UTILS } from './utils';
+// Frontend
+export { default as SIDEBAR_DEFAULT_FRONTEND } from './frontend'
+// Backend
+export { default as SIDEBAR_JAVA } from './backend/java'
+export { default as SIDEBAR_SPRING } from './backend/spring';
+export { default as SIDEBAR_DEFAULT_BACKEND } from './backend';
+// APIs
+export { default as SIDEBAR_DEFAULT_API } from './api';
+// Devops & CICD
+export { default as SIDEBAR_DEFAULT_DEVOPS } from './devops';
+// SWE
+export { default as SIDEBAR_DEFAULT_SWE } from './swe';
+// CSE
+export { default as SIDEBAR_DEFAULT_CSE } from './cse';
+// AI
+export { default as SIDEBAR_DEFAULT_AI } from './ai';
+// Misc
+export { default as SIDEBAR_DEFAULT_MISC } from './misc';
