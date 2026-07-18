d55b23d3144cf81080881b48c118f62172c948ed
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-07-18 16:07:59 +0100

    image component

--- docs/components/ImageComponent.vue
+++ docs/components/ImageComponent.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <p>Properties</p>
-        <ul>
+        <ul v-if="showDebugText">
             <li>prefix : {{ prefix }}</li>
             <li>imagePath : {{ imagePath }}</li>
             <li>imageUrl : {{ imageUrl }}</li>
         </ul>
 
-        <img :src="imageUrl" alt="Image is missing" srcset="">
+        <img :src="imageUrl" alt="Image is missing" srcset="" :title="title">
     </div>
     
 </template>
@@ -16,8 +16,10 @@
 import { ref, computed, defineProps } from 'vue';
 
 const props = defineProps({
-  prefix: {type: String, default: 'https://cdn.pixabay.com/photo'},
+  prefix: {type: String, default: 'https://raw.githubusercontent.com/arpit04tripathi/cdn/main/notebook'},
   imagePath: { type: String, required: true },
+  showDebugText: { type: Boolean, default: false },
+  title: { type: String }
 })
 
 const images = ref(
